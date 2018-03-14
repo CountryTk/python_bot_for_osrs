7fbb1e720b709b8cd1c54c6d445d2502e4bd04ba
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-03-14 17:50:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
-
 from pyautogui import *
 from PIL import Image
 from time import sleep
-
+import random
+from pymsgbox import alert
 FAILSAFE = True
 
-def main():
-    log = locateCenterOnScreen(r'images\willow_log.png')
-    knife = locateCenterOnScreen(r'images\knife2.png')
-     #gets the pic on screeen
-    #x1 , y1 = longbowlocation
-    print("Position of the knife {}".format(knife))
-    print("Position of your log {}".format(log))
-    
-    while True:
-        click(log, duration=1)
-        sleep(1)#Wont detect the image with less time
-        longbowlocation = locateCenterOnScreen(r'images\long_bow.png')
-        print(longbowlocation)
-        if longbowlocation != None:
-            click(longbowlocation, duration=0.92)
-        break
-       
-    
+class main():
+    def __init__(self):
+        self.willow_log = r'images\willow.png'
+        self.maple_log = r'images\maple_log.png'
+        self.yew_log = None
+        self.magic_log = None
+        self.knife_willows = r'images\knife_willow.png'
+        self.willow_long_bow = r'images\long_bow.png'
+        self.main() #running the main func
+
+    def willow(self):
+        duration_time = random.uniform(0.80, 1.35)
+        #print(duration_time)
+        #finding the log
+        knife = locateCenterOnScreen(self.knife_willows) 
+        log_image = locateCenterOnScreen(self.willow_log)
+        if log_image == None or self.knife_willows == None:
+            print("u fucked up, try again")
+        else:
+            click(log_image, duration=duration_time)
+            sleep(1)
+            if knife == None:
+                print('shit')
+            click(knife, duration=0.92)
+            sleep(1)
+            long_bow_image = locateCenterOnScreen(self.willow_long_bow)
+            click(long_bow_image, duration=duration_time)
+            sleep(50)
+            alert(text='Please bank your longbows and take a fresh inventory of willow logs', title='Alert!', button='OK') #It takes approx 50 seconds to fletch the whole inv
+
+    def maple(self):
+        image = locateCenterOnScreen(self.maple_log)
+        if image == None:
+            print("fuck i messed up")
+        else: 
+            click(image, duration=0.92)
+    def main(self):
+        choice = input()
+        if choice == "willow":
+            self.willow()
+        elif choice == "maple":
+            self.maple()
+        
 
 
-main()
+
+
+
+
+
+if __name__ == '__main__':
+    go = main()
+    
+    
