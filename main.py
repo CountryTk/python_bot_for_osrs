c280006489fc15b41dbd1532bad51d93c7e1a136
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-03-16 21:27:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from time import sleep
 import random
 import threading
 from pymsgbox import alert
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel, QDialog
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import pyqtSlot, QThread
 from PyQt5 import QtTest
@@ -51,7 +51,8 @@ class main(QMainWindow):
         #creating maple button
 
         button_maple = QPushButton("Maple longbows", self)
-        button_maple.clicked.connect(self.threading.maple)
+        b1 = button_maple.clicked.connect(self.threading.maple)
+        print(b1)
         button_maple.move(100,310)
 
         #creating yew longbow button
@@ -70,20 +71,20 @@ class main(QMainWindow):
         backpack_loadout_label.resize(backpack_image_label.width(), backpack_image_label.height())
         
         
-        #Creating the reccommendation label and adding text to it
+        #Creating the recommendation label and adding text to it
 
-        self.rec_label = QLabel(self)
-        self.rec_label.move(10, 260)
-        self.rec_label.resize(185,10)
-        self.rec_label.setText("You must use this backpack loadout ^")
+        rec_label= QLabel(self)
+        rec_label.move(10, 260)
+        rec_label.resize(185,10)
+        rec_label.setText("You must use this backpack loadout ^")
         
         #Python power
-        self.python_label = QLabel(self)
-        self.python_label_pixmap = QPixmap(self.python_power)
-        self.python_label.setPixmap(self.python_label_pixmap)
-        self.python_label.resize(200,160)
+        python_label = QLabel(self)
+        python_label_pixmap = QPixmap(self.python_power)
+        python_label.setPixmap(python_label_pixmap)
+        python_label.resize(200,160)
         
-        self.python_label.move(260, 180)
+        python_label.move(260, 180)
         
         self.show()
         
@@ -99,38 +100,44 @@ class Thread(QThread):
         self.knife_willows = r'images\knife_willow.png'
         self.willow_long_bow = r'images\long_bow.png'
         self.backpack_image = r'images\backpack_loadout.png'
-    
+
+
+
     def willow(self):
         duration_time = random.uniform(0.80, 1.35)
         #print(duration_time)
         #finding the log
         knife = locateCenterOnScreen(self.knife_willows) 
         log_image = locateCenterOnScreen(self.willow_log)
-        if log_image == None or self.knife_willows == None:
-            print("u fucked up, try again")
+        if log_image is None or self.knife_willows is None:
+            alert('No logs found or knife found', 'Error')
+            return "shit"
         else:
             click(log_image, duration=duration_time)
             QtTest.QTest.qWait(1000)
-            if knife == None:
-                print('shit')
+            if knife is None:
+                alert('No knife found', 'Error')
+
             click(knife, duration=0.92)
             QtTest.QTest.qWait(1000)
             long_bow_image = locateCenterOnScreen(self.willow_long_bow)
             click(long_bow_image, duration=duration_time)
-            QtTest.QTest.qWait(50000)
-            alert(text='Please bank your longbows and take a fresh inventory of willow logs', title='Alert!', button='OK') #It takes approx 50 seconds to fletch the whole inv
+            QtTest.QTest.qWait(50000) # It takes approx 50 seconds to fletch the whole in
+
 
     def maple(self):
         image = locateCenterOnScreen(self.maple_log)
-        if image == None:
-            alert('Error, maple logs not FUCKING FOUND', 'shit', 'OK')
+        if image is None:
+            alert('No logs found', 'Error')
         else: 
             click(image, duration=0.92)
+            if self.knife_willows is None:
+                alert("No knife found", "Error")
+
     def yew(self):
         pass
     def magic(self):
         pass
-
 
 
 
